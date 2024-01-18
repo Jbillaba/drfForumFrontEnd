c7f018c49190a3a641149100f149cc0a2a9c393a
jbillaba <jeremyvillalva66@gmail.com>
2024-01-18 12:05:05 -0500

    added tailwind css

--- forum-front-end/src/app/app.routes.ts
+++ forum-front-end/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { LogInPageComponent } from './log-in-page/log-in-page.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { LogInPageComponent } from './components/log-in-page/log-in-page.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     {path: '', component: LandingPageComponent},
     {path: 'login', component: LogInPageComponent},
+    {path:'**', component: PageNotFoundComponent},
 ];
