2c5242c0ad9b9c62b92657cba1672015aaa0348a
jbillaba <jeremyvillalva66@gmail.com>
2024-01-20 11:20:00 -0500

    added forms with validators on register page'

--- forum-front-end/src/app/components/log-in-page/log-in-page.component.ts
+++ forum-front-end/src/app/components/log-in-page/log-in-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-log-in-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './log-in-page.component.html',
   styleUrl: './log-in-page.component.css'
 })
--- forum-front-end/src/app/components/register-page/register-page.component.ts
+++ forum-front-end/src/app/components/register-page/register-page.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import {FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, ],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.css'
 })
 export class RegisterPageComponent {
 
+  passMinLength: number = 8;
+
+  registerForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    username: new FormControl('', Validators.required),
+    name: new FormControl(''),
+    password: new FormControl('', [Validators.required, Validators.minLength(this.passMinLength)]),
+    password2: new FormControl('',[Validators.required, Validators.minLength(this.passMinLength)]),
+
+  });
+  onSubmit(){
+    console.warn(this.registerForm.value);
+  }
 }
