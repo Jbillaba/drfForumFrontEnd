a992d78117b927adb1675a2948048e1912ef86e3
jbillaba <jeremyvillalva66@gmail.com>
2024-01-20 12:37:27 -0500

    taking a break for now

--- forum-front-end/src/app/components/register-page/register-page.component.ts
+++ forum-front-end/src/app/components/register-page/register-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ForumapiService } from '../../services/forumapi.service';
+import { CommonModule } from '@angular/common';
+import { MustMatch } from '../../_helpers';
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [ReactiveFormsModule, ],
+  imports: [ReactiveFormsModule, CommonModule ],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.css'
 })
@@ -16,14 +18,20 @@ export class RegisterPageComponent {
   passMinLength: number = 8;
 
   registerForm = new FormGroup({
-    email: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.email, Validators.required]),
     username: new FormControl('', Validators.required),
     name: new FormControl(''),
     password: new FormControl('', [Validators.required, Validators.minLength(this.passMinLength)]),
     password2: new FormControl('',[Validators.required, Validators.minLength(this.passMinLength)]),
 
+  }, {
+    validators: MustMatch('password', 'password2')
   });
+
   onSubmit(){
     this.forumApiService.postRegister(this.registerForm.value).subscribe(user => this.users.push(user))
   }
+
+  
+
 }
